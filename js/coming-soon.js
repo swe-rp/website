7a87797301bd511f024991f3c872d3b7aaa2bc17
Leslie Cheng <leslie.cheng5@gmail.com>
2019-09-16 20:41:52 -0700

    First commit.

---
+++ js/coming-soon.js
@@ -0,0 +1,8 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  $("#button").click(() => {
+    alert("Yeah uh this doesn't actually do anything.");
+  })
+
+})(jQuery); // End of use strict
